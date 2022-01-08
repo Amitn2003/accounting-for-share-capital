91825f281bc14523a6f01e54f630e6b32ab77ffe
Amitn2003 <64121569+Amitn2003@users.noreply.github.com>
2022-01-08 23:56:07 +0530

    Update accounting on issue of share(beta).py

--- accounting on issue of share(beta).py
+++ accounting on issue of share(beta).py
@@ -9,7 +9,7 @@ showing_share = int(input("Enter total  issued share ammount : "))
 
 total_payable = int(input("Enter total payable ammount : "))
 
-real_share = int(input("Receved received applications ammounts : ") or showing_share)
+real_share = int(input("Receved received applications ammounts : "))
 
 # print(f"\n \nIssued shares are {showing_share} \nPayable ammount {total_payable} \nTotal applications received {real_share}. \n")
 
@@ -22,7 +22,7 @@ real_share = int(input("Receved received applications ammounts : ") or showing_s
 app_money = int(input("Application money : "))
 allotment_money = int(input("Allotment money : "))
 firstcall = int(input("1st call money : "))
-finalcall = int(input("Final call money : "))
+finalcall = int(input("Final call money : ") or "0")
 
 
 
@@ -39,7 +39,7 @@ if everything_is_ok == everything_is_ok2:
     # time.sleep(0.7)
 elif everything_is_ok != everything_is_ok2:
     print(f"It would be better if you check all your ammounts! \nIt seems like wrong!!!!! \n")
-    time.sleep(3)
+    time.sleep(3.85)
 
 
 
@@ -186,6 +186,35 @@ def allotment_arrear(allot_arrear):
 
 
 
+def share_forfeature(forfeited_share_num):
+    shr_app_f = forfeited_share_num * total_payable
+    call_arr_all = allotment_call_arrear_ammount * allotment_money
+    call_arr_fst = first_call_arrear_ammount * firstcall
+    call_arr_fnl = finalcall * final_call_arrear_ammount
+    call_total_f = call_arr_all + call_arr_fst + call_arr_fnl
+    sec_pre_f = 0
+    print(f"\n \n \nShare Capital ....... A/C            Dr.    {shr_app_f}")
+    # Security pre -------------------------
+    if premium > 0:
+        sec_pre_f = premium * forfeited_share_num
+        print(f"Security Premium A/C  .......   Dr.          {sec_pre_f}")
+        shr_app_f = shr_app_f + sec_pre_f
+    # -----------------------
+    print(f"To Calls in Arrear....... A/C                {call_total_f}")
+    shr_frfture = shr_app_f - call_total_f 
+    print(f"To Share forfeature....... A/C                 {shr_frfture}")
+  
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -198,7 +227,7 @@ if advance == 1:
     # Checking premium on allotment
     premium = int(input("Enter the premium ammount : ") or "0")
     print(f"Total payable including premium is {premium + total_payable} .")
-    time.sleep(1)
+    #time.sleep(1)
 
     allotment_call_arrear_ammount = int(input("Enter the allotment arrear share ammount : ") or "0")
 
@@ -208,7 +237,9 @@ if advance == 1:
 
 
     final_call_arrear_ammount = int(input("Enter the final call arrear share ammount : ") or "0")
-    # final_call_arrear = int(input("Final call arrear shares:") or "0")
+    
+    forfeited_share_num = int(input("Enter Forfeated share amount : ") or "0")
+
     
 
 
@@ -218,32 +249,32 @@ if advance == 1:
 
     premium_ammount = int(premium)
     share_app()
-    time.sleep(1)
+    #time.sleep(1)#
     if showing_share < real_share:
         share_app_refund()
         real_share = showing_share # Share will calculate on issuing share
-        time.sleep(1)
+        #time.sleep(1)
     share_app_cap()
 
-    time.sleep(1)
+    #time.sleep(1)
 
 
     if premium_ammount > 0:
         share_all_premium()
-        time.sleep(1)
+        #time.sleep(1)
         if allotment_call_arrear_ammount > 0:
             allotment_arrear(allotment_call_arrear_ammount)
         share_all_premium_cap()
-        time.sleep(1)
+        #time.sleep(1)
     elif premium_ammount == 0:
         share_all()
-        time.sleep(1)
+        #time.sleep(1)
         if allotment_call_arrear_ammount > 0:
             allotment_arrear(allotment_call_arrear_ammount)
 
         elif allotment_call_arrear_ammount == 0:
             share_all_cap()
-        time.sleep(1)
+        #time.sleep(1)
     else:
         print("Premium can't be less than 0 !!")
         print(exit())
@@ -261,23 +292,26 @@ if advance == 1:
 
     else:
         first_call()
-        time.sleep(1)
+        #time.sleep(1)
         if first_call_arrear_ammount > 0:
             first_arrear(first_call_arrear_ammount)
 
         elif first_call_arrear_ammount == 0:
             first_call_cap()
 
-        time.sleep(1)
+        #time.sleep(1)
         final_call()
-        time.sleep(1)
+        #time.sleep(1)
         if final_call_arrear_ammount > 0:
             final_arrear(final_call_arrear_ammount)
 
 
         elif final_call_arrear_ammount == 0:
             final_call_cap()
-            time.sleep(1)
+            #time.sleep(1)
+
+        
+        share_forfeature(forfeited_share_num)
 
 
 
@@ -287,20 +321,20 @@ elif advance == 0:
     share_app()
 
 
-    time.sleep(1)
+    time.sleep(0.25)
     if showing_share < real_share:
         share_app_refund()
         real_share = showing_share # Share will calculate on issuing share
-        time.sleep(1)
+        #time.sleep(1)
     share_app_cap()
 
-    time.sleep(1)
+    #time.sleep(1)
 
     share_all()
-    time.sleep(1)
+    #time.sleep(1)
 
     share_all_cap()
-    time.sleep(1)
+    #time.sleep(1)
 
 
     if firstcall == 0 or finalcall == 0:
@@ -310,7 +344,7 @@ elif advance == 0:
             print(f"To Share Capital A/C                     {real_share * more_among_fst_fnl_call()}")
 
 
-            time.sleep(1)
+           # time.sleep(1)
             print(f"\n \nBank A/C ......... Dr.               {real_share *     more_among_fst_fnl_call()}")
             print(f"To Share First & Final call A/C                 {real_share *  more_among_fst_fnl_call()}")
 
@@ -320,25 +354,25 @@ elif advance == 0:
             print(f"To Share Capital A/C                     {real_share * more_among_fst_fnl_call()}")
 
 
-            time.sleep(1)
+           # time.sleep(1)
             print(f"\n \nBank A/C ......... Dr.               {real_share *     more_among_fst_fnl_call()}")
             print(f"To Share First & Final call A/C                 {real_share *  more_among_fst_fnl_call()}")
 
 
     else:
         first_call()
-        time.sleep(1)
+        #time.sleep(1)
 
     
         first_call_cap()
-        time.sleep(1)
+        #time.sleep(1)
 
 
 
         final_call()
-        time.sleep(1)
+        #time.sleep(1)
         final_call_cap()
-        time.sleep(1)
+        #time.sleep(1)
 
 
 
@@ -357,9 +391,9 @@ else:
 
 
 print("\nThank you for using Issue of share tool...")
-time.sleep(2)
+time.sleep(2.1)
 print("Exiting...")
-time.sleep(1)
+time.sleep(1.25)
 
 print(exit())
 
