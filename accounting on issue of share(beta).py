ff8c3bb9e199392077d2782a9aabfc3962257258
Amitn2003 <64121569+Amitn2003@users.noreply.github.com>
2022-01-08 23:21:25 +0530

    Update accounting on issue of share(beta).py

--- accounting on issue of share(beta).py
+++ accounting on issue of share(beta).py
@@ -11,15 +11,9 @@ total_payable = int(input("Enter total payable ammount : "))
 
 real_share = int(input("Receved received applications ammounts : ") or showing_share)
 
-print(f"\n \nIssued shares are {showing_share} \nPayable ammount {total_payable} \nTotal applications received {real_share}. \n")
+# print(f"\n \nIssued shares are {showing_share} \nPayable ammount {total_payable} \nTotal applications received {real_share}. \n")
 
 
-# if showing_share >= real_share:
-#     print("Everything is ok!")
-
-# else:
-#     print("Check your input!!!!!!")
-#     time.sleep(8)
 
 
 
@@ -37,18 +31,24 @@ advance = int(input("Advance control ? [Type 1 or 0(default 0)} :") or "0")
 
 
 
+everything_is_ok = app_money + allotment_money + firstcall + finalcall 
+everything_is_ok2 = total_payable
 
-
+if everything_is_ok == everything_is_ok2:
+    print(f"All ammounts are valid. Thank you. \n")
+    # time.sleep(0.7)
+elif everything_is_ok != everything_is_ok2:
+    print(f"It would be better if you check all your ammounts! \nIt seems like wrong!!!!! \n")
+    time.sleep(3)
 
 
 
 
 # Receving applications 
 def share_app():
-    receved_money = real_share * app_money
-    print(f"\n \n \n \nBank A/C ....... Dr.            {receved_money}")
-    print(f"To Share Application A/C                  {receved_money}")
-    return receved_money
+    mny_recvd_app = real_share * app_money
+    print(f"\n \n \n \nBank A/C ....... Dr.            {mny_recvd_app}")
+    print(f"To Share Application A/C                  {mny_recvd_app}")
 
 
 # Refunding if receving application ammount is gretter than issued application
@@ -57,97 +57,85 @@ def share_app_refund():
     refund = refund_share_no * app_money
     print(f"\n  \nShare Application A/C ...... Dr.       {refund}")
     print(f"To Back A/C ......... Dr.                   {refund}")
-    return refund
+
 
 # Share Application money transfurring to Share Capital A/C
 def share_app_cap():
-    transferred_application = real_share * app_money
-    print(f"\n \nShare Application A/C ...... Dr.       {transferred_application}")
-    print(f"To Share Capital A/C                     {transferred_application}")
-    return transferred_application
-
-
+    money_receved_application = real_share * app_money
+    print(f"\n \nShare Application A/C ...... Dr.       {money_receved_application}")
+    print(f"To Share Capital A/C                     {money_receved_application}")
 
 
 # Share Allotment money demanding
 def share_all():
-    due_allotment = real_share * allotment_money
-    print(f"\n \nShare Allotment A/C ....... Dr.       {due_allotment}")
-    print(f"To Share Capital A/C                     {due_allotment}")
-    return due_allotment
+    shr_allot = real_share * allotment_money
+    print(f"\n \nShare Allotment A/C ....... Dr.       {shr_allot}")
+    print(f"To Share Capital A/C                     {shr_allot}")
 
 
 # Share Allotment money receveing from bank
 def share_all_cap():
-    transferred_allotment = real_share * allotment_money
-    print(f"\n \nBank A/C ........ Dr.              {transferred_allotment}")
-    print(f"To Share Allotment A/                   {transferred_allotment}")
-    return transferred_allotment
+    shr_allot = real_share * allotment_money
+    print(f"\n \nBank A/C ........ Dr.              {shr_allot}")
+    print(f"To Share Allotment A/                   {shr_allot}")
 
 
 
 # Share 1stcall monney demanding
 def first_call():
-    due_first = real_share * firstcall
-    print(f"\n \nShare 1st call A/C .... ... Dr.      {due_first}")
-    print(f"To Share Capital A/C                     {due_first}")
-    return  due_first 
+    shr_fst = real_share * firstcall
+    print(f"\n \nShare 1st call A/C .... ... Dr.      {shr_fst}")
+    print(f"To Share Capital A/C                     {shr_fst}")
+
 
 
 
 # Share 1stcall monney receveing from bank
 def first_call_cap():
-    transferred_first = real_share * firstcall
-    print(f"\n \nBank A/C ......... Dr.              {transferred_first}")
-    print(f"To Share 1st call A/C                   {transferred_first}")
-    return transferred_first
+    shr_fst = real_share * firstcall
+    print(f"\n \nBank A/C ......... Dr.              {shr_fst}")
+    print(f"To Share 1st call A/C                   {shr_fst}")
 
 
 
 # Share Final call monney demanding
 def final_call():
-    due_final = real_share * finalcall
-    print(f"\n \nShare Final call A/C ...... Dr.      {due_final}")
-    print(f"To Share Capital A/C                     {due_final}")
-    return due_final
+    shr_fnl = real_share * finalcall
+    print(f"\n \nShare Final call A/C ...... Dr.      {shr_fnl}")
+    print(f"To Share Capital A/C                     {shr_fnl}")
 
 
 
 # Share 1stcall monney receveing from bank
 def final_call_cap():
-    transferred_final = real_share * finalcall
-    print(f"\n \nBank A/C ......... Dr.               {transferred_final}")
-    print(f"To Share Final call A/C                 {transferred_final}")
-    return transferred_final
+    shr_fnl = real_share * finalcall
+    print(f"\n \nBank A/C ......... Dr.               {shr_fnl}")
+    print(f"To Share Final call A/C                 {shr_fnl}")
 
 
 # Share allotment including premium demanding
 def share_all_premium():
     all_pre = allotment_money + premium
-    total_all = real_share * all_pre
-    total_cap = real_share * allotment_money
-    only_premium = real_share * premium
-    print(f"\n \nShare Allotment A/C ....... Dr.          {total_all}")
-    print(f"To Share Capital A/C                        {total_cap}")
-    print(f"To Security Premium A/C                     {only_premium}")
-    return (total_all, total_cap, only_premium)
+    all_premium = real_share * all_pre
+    print(f"\n \nShare Allotment A/C ....... Dr.          {all_premium}")
+    print(f"To Share Capital A/C                        {real_share * allotment_money}")
+    print(f"To Security Premium Reserve A/C                     {real_share * premium}")
 
 
 # Share allotment money including premium receving
 def share_all_premium_cap():
     all_pre = allotment_money + premium
     all_premium = real_share * all_pre
+    # all_premium = real_share * allotment_money
+    
     print(f"\n \nBank A/C ........ Dr.              {all_premium}")
     print(f"To Share Allotment A/                   {all_premium}")
-    return all_premium
-
 
 
 def first_final():
-    first_final = real_share * finalcall
-    print(f"\n \nShare First & Final call A/C ...... Dr.      {first_final}")
-    print(f"To Share Capital A/C                     {first_final}")
-    return first_final
+    shr_finl = real_share * finalcall
+    print(f"\n \nShare First & Final call A/C ...... Dr.      {shr_finl}")
+    print(f"To Share Capital A/C                     {shr_finl}")
 
 
 
@@ -158,20 +146,41 @@ def more_among_fst_fnl_call():
         return firstcall
 
 
+def more_among_fst_fnl_call_arrear():
+    if first_call_arrear_ammount < final_call_arrear_ammount:
+        return final_call_arrear_ammount
+    elif first_call_arrear_ammount > final_call_arrear_ammount:
+        return first_call_arrear_ammount
 
 
 
+def final_arrear(final_arrear):
+    shr_fnl = real_share * finalcall
+    calls = finalcall * final_arrear
+    bank = shr_fnl - calls
+    print(f"\n \nBank A/C ......... Dr.               {bank}")
+    print(f"Calls in Arrear A/C ....... Dr.             {calls}")
+    print(f"To Share Final call A/C                 {shr_fnl}")
 
 
 
+def first_arrear(first_arrear):
+    shr_fnl = real_share * firstcall
+    calls = firstcall * first_arrear
+    bank = shr_fnl - calls
+    print(f"\n \nBank A/C ......... Dr.               {bank}")
+    print(f"Calls in Arrear A/C ....... Dr.             {calls}")
+    print(f"To Share Final call A/C                 {shr_fnl}")
 
 
-
-
-
-
-
-
+def allotment_arrear(allot_arrear):
+    shr_allot = real_share * allotment_money
+    calls = allotment_money * allot_arrear
+    
+    bank = shr_allot - calls
+    print(f"\n \nBank A/C ......... Dr.               {bank}")
+    print(f"Calls in Arrear A/C ....... Dr.             {calls}")
+    print(f"To Share Final call A/C                 {shr_allot}")
 
 
 
@@ -190,8 +199,23 @@ if advance == 1:
     premium = int(input("Enter the premium ammount : ") or "0")
     print(f"Total payable including premium is {premium + total_payable} .")
     time.sleep(1)
+
+    allotment_call_arrear_ammount = int(input("Enter the allotment arrear share ammount : ") or "0")
+
+
+
+    first_call_arrear_ammount = int(input("Enter the first call arrear share ammount : ") or "0")
+
+
+    final_call_arrear_ammount = int(input("Enter the final call arrear share ammount : ") or "0")
     # final_call_arrear = int(input("Final call arrear shares:") or "0")
     
+
+
+
+
+
+
     premium_ammount = int(premium)
     share_app()
     time.sleep(1)
@@ -204,15 +228,21 @@ if advance == 1:
     time.sleep(1)
 
 
-    if premium_ammount != 0:
+    if premium_ammount > 0:
         share_all_premium()
         time.sleep(1)
+        if allotment_call_arrear_ammount > 0:
+            allotment_arrear(allotment_call_arrear_ammount)
         share_all_premium_cap()
         time.sleep(1)
     elif premium_ammount == 0:
         share_all()
         time.sleep(1)
-        share_all_cap()
+        if allotment_call_arrear_ammount > 0:
+            allotment_arrear(allotment_call_arrear_ammount)
+
+        elif allotment_call_arrear_ammount == 0:
+            share_all_cap()
         time.sleep(1)
     else:
         print("Premium can't be less than 0 !!")
@@ -232,30 +262,26 @@ if advance == 1:
     else:
         first_call()
         time.sleep(1)
-        first_call_cap()
+        if first_call_arrear_ammount > 0:
+            first_arrear(first_call_arrear_ammount)
+
+        elif first_call_arrear_ammount == 0:
+            first_call_cap()
+
         time.sleep(1)
         final_call()
         time.sleep(1)
-        final_call_cap()
-        time.sleep(1)
+        if final_call_arrear_ammount > 0:
+            final_arrear(final_call_arrear_ammount)
+
+
+        elif final_call_arrear_ammount == 0:
+            final_call_cap()
+            time.sleep(1)
 
 
 
 
-      # if finalcall > 0:
-      #   final_call()
-      #   time.sleep(1)
-     #   first_call_cap()
-      
-      
-      
-
-    #    print("\nThank you for using Issue of share tool...")
-    #     time.sleep(2)
-    # print("Exiting...")
-    #     time.sleep(1)
-
-    #    print(exit())
 elif advance == 0:
     print("You are using normal version.")
     share_app()
@@ -276,20 +302,43 @@ elif advance == 0:
     share_all_cap()
     time.sleep(1)
 
-    first_call()
-    time.sleep(1)
 
-    first_call_cap()
-    time.sleep(1)
+    if firstcall == 0 or finalcall == 0:
+        if first_call_arrear_ammount == 0 and final_call_arrear_ammount == 0:
 
-    # if finalcall > 0:
-    #   final_call()
-    #   time.sleep(1)
-    #   first_call_cap()
-    final_call()
-    time.sleep(1)
-    final_call_cap()
-    time.sleep(1)
+            print(f"\n \nShare  First & Final call A/C ...... Dr.      {real_share * more_among_fst_fnl_call()}")
+            print(f"To Share Capital A/C                     {real_share * more_among_fst_fnl_call()}")
+
+
+            time.sleep(1)
+            print(f"\n \nBank A/C ......... Dr.               {real_share *     more_among_fst_fnl_call()}")
+            print(f"To Share First & Final call A/C                 {real_share *  more_among_fst_fnl_call()}")
+
+        elif first_call_arrear_ammount != 0 or final_call_arrear_ammount != 0:
+            print(f"\n \nShare  First & Final call A/C ...... Dr.      {real_share * more_among_fst_fnl_call()}")
+            print(f"Calls in arrear A/C .... Dr.                 {more_among_fst_fnl_call_arrear() * more_among_fst_fnl_call()}")
+            print(f"To Share Capital A/C                     {real_share * more_among_fst_fnl_call()}")
+
+
+            time.sleep(1)
+            print(f"\n \nBank A/C ......... Dr.               {real_share *     more_among_fst_fnl_call()}")
+            print(f"To Share First & Final call A/C                 {real_share *  more_among_fst_fnl_call()}")
+
+
+    else:
+        first_call()
+        time.sleep(1)
+
+    
+        first_call_cap()
+        time.sleep(1)
+
+
+
+        final_call()
+        time.sleep(1)
+        final_call_cap()
+        time.sleep(1)
 
 
 
@@ -307,9 +356,6 @@ else:
 
 
 
-
-
-
 print("\nThank you for using Issue of share tool...")
 time.sleep(2)
 print("Exiting...")
@@ -320,62 +366,6 @@ print(exit())
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# share_app()
-
-
-# time.sleep(1)
-# if showing_share < real_share:
-#     share_app_refund()
-#     real_share = showing_share # Share will calculate on issuing share
-#     time.sleep(1)
-
-
-
-# share_app_cap()
-
-# time.sleep(1)
-
-# share_all()
-# time.sleep(1)
-
-# share_all_cap()
-# time.sleep(1)
-
-# first_call()
-# time.sleep(1)
-
-# first_call_cap()
-# time.sleep(1)
-
-# if finalcall > 0:
-#     final_call()
-#     time.sleep(1)
-#     first_call_cap()
-# time.sleep(1)
-
-
-# print("\nThank you for using Issue of share tool...")
-# time.sleep(2)
-# print("Exiting...")
-# time.sleep(1)
-
-# print(exit())
 
 
 
